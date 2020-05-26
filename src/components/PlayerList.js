3bd3cbf41d8f17a6e6cddcc2bab8c5c2600be3d6
devmullets <devmullets@gmail.com>
2020-05-26 11:28:12 -0400

    initial commit

---
+++ src/components/PlayerList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PlayerList extends Component {
+    render() {
+        return (
+            <div>
+                I'm a playerlist
+            </div>
+        );
+    }
+}
+
+export default PlayerList;
---
+++ src/components/PlayerPage.js
@@ -0,0 +1,15 @@
+// import React, { Component } from 'react';
+import React, {Component } from 'react';
+// import PlayerList from './components/PlayerList';
+import PlayerList from './PlayerList'
+
+class PlayerPage extends Component {
+    render() {
+        return (
+            // <NavBar />
+            <PlayerList />
+        )
+    }
+}
+
+export default PlayerPage;
