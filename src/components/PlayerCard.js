122cc39ff14cec7ce99b7091a93641a7dbba3675
devmullets <devmullets@gmail.com>
2020-05-28 09:38:47 -0400

    update player cards

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,41 +1,39 @@
 import React, { Component } from 'react';
 import Card from "react-bootstrap/Card";
+import { Button } from 'react-bootstrap'
 import SinglePlayer from './SinglePlayer';
 
 
 class PlayerCard extends Component {
     state = {
-        show: false
+        cardToggle: false,
     }
 
-    handleClick (e) {
-        e.preventDefault()
-        if(this.state.show) {
-            this.setState({show: false})
-        }
-        else {
-            this.setState({show:true})
-        }
+    handleClick = () => {
+        // set the state of cardToggle to opposite of whatever it is currently
+        this.setState({ cardToggle: !this.state.cardToggle})
     }
 
+    
     render(){
-        let info = <div> </div>
-        if(this.state.show){
-            info = <SinglePlayer player={this.props.player}/>
-        }
-
-     return(
-         
-        <Card className="player-card" onClick={this.handleClick.bind(this)}>
-             {info}
-             <Card.Title>
-                {this.props.player.name}
-             </Card.Title>
-
-             <Card.Text>
-                {this.props.player.team}
-             </Card.Text>
-         </Card>
+      
+     return(  
+        <Card>
+            <Card.Body>
+                <Card.Title>{this.props.player.name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{this.props.player.team}</Card.Subtitle>
+                <Card.Text>
+                    {/* if cardToggle = TRUE then display SinglePlayer, if FALSE do nothing */}
+                    {this.state.cardToggle ? <SinglePlayer player={this.props.player}/> : null }
+                </Card.Text>
+                <Button variant="outline-success">
+                    Add to Team
+                </Button>{'  '}
+                <Button variant="outline-info" onClick={this.handleClick}>
+                    {this.state.cardToggle ? "Hide Stats" : "View Stats"}    
+                </Button>
+            </Card.Body>
+        </Card>
      )}
 }
 
